1b75c81c09cae081a2ed8e998532ddaeed52d455
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2024-06-22 17:51:39 +0530

    basic calculator

---
+++ Function_Variables/06_calculator.py
@@ -0,0 +1,10 @@
+def calculator(x,y):
+    product =x*y
+    return product
+
+def main():
+    x=int(input("Enter the number-"))
+    y=int(input("Enter the number-"))
+    print("Product =", calculator(x,y))
+
+main()
